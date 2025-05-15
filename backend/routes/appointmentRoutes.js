f64387dfa133330546924f9dc2df47091be25898
Pablooca <pabloocagal03@gmail.com>
2025-05-15 19:09:56 +0300

    update

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {getAppointments, getAppointmentByUser, getAppointmentByWorker, getAppointmentByDate, createAppointment, delAppointment, updateAppointment} = require('../controllers/appointmentController');
+const { protect_user, protect_worker } = require('../middleware/authMiddleware');
 
-router.route('/user/:user_id').get(getAppointmentByUser);
-router.route('/worker/:worker_id').get(getAppointmentByWorker);
-router.route('/date/:date').get(getAppointmentByDate);
-router.route('/').get(getAppointments).post(createAppointment);
-router.route('/:id').delete(delAppointment).put(updateAppointment);
+
+router.route('/user/:user_id').get(protect_user, getAppointmentByUser);
+router.route('/worker/:worker_id').get(protect_worker, getAppointmentByWorker);
+router.route('/date/:date').get(protect_worker, getAppointmentByDate);
+router.route('/').get(protect_worker, getAppointments).post(protect_worker, createAppointment);
+router.route('/:id').delete(protect_worker, delAppointment).put(protect_worker, updateAppointment);
 
 module.exports = router;
